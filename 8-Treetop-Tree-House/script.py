b01b4d2704acc59cc5344c6b94c5b22b46247d10
Marcus Kofal <kofalm@CSGP.EDU>
2022-12-13 14:23:25 -0700

    progress on day 8

---
+++ 8-Treetop-Tree-House/script.py
@@ -0,0 +1,22 @@
+with open("8-Treetop-Tree-House/input.txt", "r") as file:
+    grid = file.read().strip().split()
+
+def part_one():
+    count = 0
+    for r in range(1, len(grid)-1):
+        for c in range(1, len(grid[r])-1):
+            #        up,            down,         left,         right
+            nearby = [grid[r-1][c], grid[r+1][c], grid[r][c-1], grid[r][c+1]]
+            for value in nearby:
+                if value > grid[r][c]:
+                    break
+            else:
+                count += 1
+            
+    print(count)
+            
+
+
+
+
+part_one()
