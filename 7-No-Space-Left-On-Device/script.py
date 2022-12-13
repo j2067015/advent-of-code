9bd1a0fca3736c4f587c9ecb1c8443f1f50f2c33
Marcus Kofal <kofalm@CSGP.EDU>
2022-12-13 14:02:14 -0700

    struggling on day 7

--- 7-No-Space-Left-On-Device/script.py
+++ 7-No-Space-Left-On-Device/script.py
@@ -1,17 +1,31 @@
 with open("7-No-Space-Left-On-Device/input.txt", "r") as file:
-    fd = file.read().strip().split("\n")
+    fd = file
+
+directories = []
 
 def part_one():
-    for line in fd:
-        print(line)
-        if line[0] == '$':
-            line = line.split(" ")
-            match line[0]:
-                case "ls":
+    while 1:
+        line = fd.readline().strip("\n").replace("$ ", "").split(" ")
+        match line[0]:
+            case "ls":
+                while 1:
+                    dir_search = fd.readline().strip("\n")
+                    if dir_search[0] == "$":
+                        break
+                    dir_search = dir_search.split(" ")
+            case "cd":
 
-                case "cd":
-                    
-        else:
-            line = line.split(" ")
+            case "dir":
+
+
+            case _:
+
+
+            
+
+
+
+def add_to_dir():
+
 
 part_one()
