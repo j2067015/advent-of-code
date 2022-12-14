849b33f446ddaba97cc91ee0a4d8fc2e3527dc58
Marcus Kofal <kofalm@CSGP.EDU>
2022-12-14 15:41:39 -0700

    backup before winter break

--- 8-Treetop-Tree-House/script.py
+++ 8-Treetop-Tree-House/script.py
@@ -3,8 +3,8 @@ with open("8-Treetop-Tree-House/input.txt", "r") as file:
 
 def part_one():
     count = 0
-    for r in range(1, len(grid)-1):
-        for c in range(1, len(grid[r])-1):
+    for r in range(1, len(grid)-2):
+        for c in range(1, len(grid[r])-2):
             #        up,            down,         left,         right
             nearby = [grid[r-1][c], grid[r+1][c], grid[r][c-1], grid[r][c+1]]
             for value in nearby:
